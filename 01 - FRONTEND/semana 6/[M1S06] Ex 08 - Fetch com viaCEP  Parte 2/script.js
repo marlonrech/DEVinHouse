a1e0e20285f0ca2b436ea1035d6adcf020c15a3e
marlonrech <68797018+marlonrech@users.noreply.github.com>
2022-08-06 14:13:11 -0300

    [M1S06] Ex 08 - Fetch com viaCEP | Parte 2
    Após implementar o Ex 3, vamos adicionar tratativas na função criada;
    
    Quando o usuário clicar no botão de buscar sem ter informado um valor no input exibir modal de alerta (alert) informando que o campo precisa ser preenchido
    Quando o usuário preencher o campo com menos ou mais de 8 caracteres deve ser exibido modal de alerta (alert) informando que o campo foi preenchido com um cep inválido
    Em ambos os casos à cima não deve ser realizada a consulta na api de cep

---
+++ 01 - FRONTEND/semana 6/[M1S06] Ex 08 - Fetch com viaCEP  Parte 2/script.js
@@ -0,0 +1,38 @@
+let meuCEP, btn;
+
+btn = document.querySelector("#btnEnviaCep");
+btn.addEventListener("click", recebeInfoDeCep);
+
+
+function recebeInfoDeCep() {
+    meuCEP = document.querySelector("#enviaCep").value;
+
+    if (meuCEP == '') {
+        alert ('Campo precisa ser preenchido')
+    }   else if (meuCEP.length != 8)  {
+        alert ('Campo foi preenchido com cep inválido')
+    }       
+    
+    const options = {
+      method: "GET",
+    };
+    fetch(`https://viacep.com.br/ws/${meuCEP}/json/`, options)
+      .then((response) => {
+        return response.json();
+      })
+      .then((infoDoCEP) => {
+        console.log(infoDoCEP);
+        let divInfo = document.querySelector("#mostraInfoCep");
+        divInfo.innerHTML = `Seu bairro é: ${infoDoCEP.bairro} <br>`;
+        divInfo.innerHTML += `Sua rua é: ${infoDoCEP.logradouro} <br>`;
+        divInfo.innerHTML += `Sua cidade é: ${infoDoCEP.localidade} <br>`;
+        divInfo.innerHTML += `Seu estado é: ${infoDoCEP.uf} <br>`;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+      
+  }
+  
+  
+ 
