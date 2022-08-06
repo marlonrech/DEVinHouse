75e2b1e8b41c00de196b5df28e931327880763e0
marlonrech <68797018+marlonrech@users.noreply.github.com>
2022-08-06 10:27:20 -0300

    [M1S06] Ex 01 - Template strings
    Em um prompt capture o seu nome, idade e o sobrenome, após isso usando o template string junte para a string abaixo.
    
    "Eu, nome sobrenome, possuo idade anos, e nasci em calculo do ano em que estamos - sua idade . "

---
+++ 01 - FRONTEND/semana 6/[M1S06] Ex 01 - Template strings/index.js
@@ -0,0 +1,9 @@
+let nome = window.prompt ('Digite seu nome: ');
+
+let idade = window.prompt ('Digite sua idade: ');
+
+let sobrenome = window.prompt ('Digite seu sobrenome: ');
+
+
+
+console.log(`Eu, ${nome} ${sobrenome}, possuo ${idade} anos, e nasci em ${2022 - idade}`);
